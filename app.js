59b4480cd145189c887be62965db51caf07d3423
TSABET2004 <127050347+TSABET2004@users.noreply.github.com>
2023-05-31 18:04:20 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const form = document.querySelector('form');
-const card_1 = document.querySelector('.container');
-const card_2 = document.querySelector('.card');
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".container");
+const card_2 = document.querySelector(".card ");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     card_1.classList.add("hel");
